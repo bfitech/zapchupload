db006a5acdd42dcae07b3fc365a8e3c75cbf20e2
Rendra Suroso <rs@cogsci.bandungfe.net>
2017-03-29 20:59:03 +0700

    Remove trailing hardcoded path.

--- tests/htdocs-test/index.php
+++ tests/htdocs-test/index.php
@@ -7,7 +7,7 @@ use BFITech\ZapCore as zc;
 use BFITech\ZapTemplate as zt;
 use BFITech\ZapChupload as zu;
 
-define('TOPDIR', '/mnt/ramdisk/zapchupload-test');
+define('TOPDIR', __DIR__ . '/uploads');
 if (!is_dir(TOPDIR))
 	mkdir(TOPDIR, 0755);
 
