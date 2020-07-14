77fd02615cd260892f86d235369ae56e1f949584
Rendra Suroso <rs@cogsci.bandungfe.net>
2020-07-14 22:15:49 +0700

    Move sample app to /demo, rewrite in Mithril.

--- demo/index.php
+++ demo/index.php
@@ -1,9 +1,11 @@
 <?php
 
 
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 
+use BFITech\ZapCore\Common;
+use BFITech\ZapCore\Config;
 use BFITech\ZapCore\Router;
 use BFITech\ZapCore\Logger;
 use BFITech\ZapTemplate\Template;
@@ -11,54 +13,166 @@ use BFITech\ZapChupload\ChunkUpload;
 
 
 define('TOPDIR', __DIR__ . '/uploads');
-define('CHUNK_SIZE', 1024 * 300);
-define('MAX_FILESIZE', 1024 * 1024 * 500);
 
 
-class ChunkRegular extends ChunkUpload {
+class ChunkUploadDemo extends ChunkUpload {
 
-	public function route_home(array $args) {
-		$core = self::$core;
+	public static $datadir;
 
-		$get = $args['get'];
-		if (empty($get)) {
-			$core::start_header(200, 3600);
-			return (new Template())->load(__DIR__ . '/home.php',
-				$this->get_config());
-		}
-
-		$file = $get['file'] ?? null;
-		if ($file)
-			$core->static_file(TOPDIR . '/data/' . $file);
-
+	protected function intercept_response(
+		int &$errno, array &$data=null, int &$http_code=200
+	) {
+		if ($errno !== 0)
+			return true;
+		if (!isset($data['done']))
+			return true;
+		$fname = self::$datadir . '/' . $data['path'];
+		# add additional attributes
+		$data['size'] = filesize($fname);
+		$data['modified'] = gmdate(DATE_ATOM, filemtime($fname));
+		$data['content_type'] = Common::get_mimetype($fname);
+		return true;
 	}
-
-	// just put a prefix 'route_*' to comply with zapcore convention
-	public function route_upload(array $args) {
-		$this->upload($args);
-	}
-
 }
 
 class Web {
+
+	const FILE_NOT_FOUND = 0x0400;
+	const DELETE_FAILED = 0x0401;
+
+	public static $cnf;
+	public static $core;
+	public static $chup;
 
 	public function __construct() {
 		if (!is_dir(TOPDIR))
 			mkdir(TOPDIR, 0755);
 
-		$logger = new Logger(
-			Logger::DEBUG, TOPDIR . '/zapchupload.log');
-		$core = (new Router)->config('logger', $logger);
+		$this->prepare_config();
+		$gcf = function($key) {
+			return self::$cnf->get($key);
+		};
 
-		$chup = new ChunkRegular(
-			$core, TOPDIR . '/temp', TOPDIR . '/data',
-			null, CHUNK_SIZE, MAX_FILESIZE, $logger
-		);
+		$log = new Logger(
+			constant('BFITech\ZapCore\Logger::' . $gcf('log.level')),
+		   	$gcf('log.file'));
+		$core = self::$core = (new Router)
+			->config('logger', $log);
 
-		$core->route('/', [$chup, 'route_home']);
-		$core->route('/upload', [$chup, 'route_upload'], 'POST');
+		$destdir = $gcf('dir.dest');
+		$chup = self::$chup = new ChunkUploadDemo(
+			$core, $gcf('dir.temp'), $destdir,
+			'demo', $gcf('size.chunk'), $gcf('size.max'), $log);
+		$chup::$datadir = $destdir;
+
+		$this->run();
+	}
+
+	private function prepare_config() {
+		$cnfile = TOPDIR . '/demo.json';
+		if (!file_exists($cnfile)) {
+			file_put_contents($cnfile, '{}');
+			$cnf = new Config($cnfile);
+			$cnf->add('dir.temp', TOPDIR . '/temp');
+			$cnf->add('dir.dest', TOPDIR . '/data');
+			$cnf->add('size.chunk', 1024 * 300);
+			$cnf->add('size.max', 1024 * 1024 * 20);
+			$cnf->add('log.level', 'DEBUG');
+			$cnf->add('log.file', TOPDIR . '/demo.log');
+		} else {
+			$cnf = new Config($cnfile);
+		}
+		self::$cnf = $cnf;
+	}
+
+	public function route_home(array $args) {
+		$mithril = '//cdnjs.cloudflare.com/ajax/libs/mithril/' .
+			'2.0.4/mithril.min.js';
+		self::$core::start_header(200, 30);
+		echo <<<EOD
+<!doctype html>
+<html>
+<head>
+	<meta name=viewport
+		content='width=device-width, initial-scale=1.0'>
+	<title>zapchupload demo</title>
+	<link href=./static/style.css rel=stylesheet>
+</head>
+<body>
+<div id=wrap>
+	<div id=box></div>
+</div>
+<script src=$mithril></script>
+<script src=./static/script.js></script>
+EOD;
+		self::$core::halt();
+	}
+
+	public function route_static(array $args) {
+		$fname = __DIR__ . '/static/' . $args['params']['path'];
+		self::$core->static_file($fname, [
+			'code' => 200,
+			'cache' => 60,
+			'reqheaders' => $args['header'],
+		]);
+	}
+
+	public function route_upload(array $args) {
+		self::$chup->upload($args);
+	}
+
+	public function route_download(array $args) {
+		$fname = self::$chup::$datadir . '/' . $args['get']['fname'];
+		self::$core->static_file($fname, [
+			'code' => 200,
+			'cache' => 3600,
+			'reqheaders' => $args['header'],
+		]);
+	}
+
+	public function route_remove(array $args) {
+		$core = self::$core;
+		$path = self::$chup::$datadir . '/' . $args['delete'];
+		if (!file_exists($path))
+			$core::pj([self::FILE_NOT_FOUND, null], 403);
+		if (!@unlink($path))
+			$core::pj([self::DELETE_FAILED, null], 403);
+		$core::pj([0, null], 200);
+	}
+
+	public function route_list(array $args) {
+		$list = [];
+		foreach (glob(self::$chup::$datadir . '/*') as $fname) {
+			if (is_dir($fname))
+				continue;
+			$list[] = basename($fname);
+		}
+		return self::$core::pj([0, $list]);
+	}
+
+	public function route_config(array $args) {
+		return self::$core::pj([0, self::$chup->get_config()]);
+	}
+
+	private function run() {
+		self::$core
+			->route('/',
+				[$this, 'route_home'])
+			->route('/static',
+				[$this, 'route_static'])
+			->route('/config',
+				[$this, 'route_config'])
+			->route('/list',
+				[$this, 'route_list'])
+			->route('/upload',
+				[$this, 'route_upload'], 'POST')
+			->route('/download',
+				[$this, 'route_download'])
+			->route('/remove',
+				[$this, 'route_remove'], 'DELETE');
 	}
 
 }
+
 
 new Web;
---
+++ demo/static/script.js
@@ -0,0 +1,372 @@
+
+/* global m */
+/* eslint no-undef: "error" */
+
+class ChuploadError {
+	constructor(errno, errmsg) {
+		this.errno = errno;
+		this.errmsg = errmsg;
+	}
+}
+
+const E_FILESLICE_NOT_FOUND = 0x0100;
+const E_UPLOAD_STARTED = 0x0101;
+const E_UNFINISHED = 0x0102;
+
+class Chupload {
+
+	constructor(
+		file, url,
+		pfx, chunkSz,
+		cbChunkOk, cbFileOk, cbError,
+		cbProgress, chunkFingerprint, postData
+	) {
+		this.file = file;
+
+		this.url = url;
+		this.postPrefix = pfx;
+		this.chunkSz = chunkSz;
+
+		this.cbChunkOk = cbChunkOk || (() => {});
+		this.cbFileOk = cbFileOk || (() => {});
+		this.cbError = cbError || (() => {});
+		this.cbProgress = cbProgress || (() => {});
+
+		this.chunkFingerprint = chunkFingerprint || (() => {});
+
+		this.postData = postData;
+
+		this.chunkIndex = -1;
+		this.chunkMax = -1;
+
+		this.progress = -1;
+		this.started = false;
+		this.cancelled = false;
+
+		// check browser compatibility
+		this.slicer = null;
+		if ('slice' in this.file)
+			this.slicer = 'slice';
+		else if ('mozSlice' in this.file)
+			this.slicer = 'mozSlice';
+		else if ('webkitSlice' in this.file)
+			this.slicer = 'webkitSlice';
+		else
+			throw new ChuploadError(
+				E_FILESLICE_NOT_FOUND, "fileSlice not supported");
+	}
+
+	start() {
+		if (this.started)
+			throw new ChuploadError(
+				E_UPLOAD_STARTED, "upload already started");
+
+		// calculate chunk size
+		this.chunkMax = Math.floor(this.file.size / this.chunkSz);
+		this.chunkIndex = 0;
+
+		this.uploadChunk();
+	}
+
+	uploadChunk() {
+		const bgn = this.chunkSz * this.chunkIndex;
+		const end = bgn + this.chunkSz;
+		const blob = this.file[this.slicer](bgn, end);
+
+		const form = new FormData();
+		const pfx = this.postPrefix;
+		form.append(pfx + 'name', this.file.name);
+		form.append(pfx + 'size', this.file.size);
+		form.append(pfx + 'index', this.chunkIndex);
+		form.append(pfx + 'blob', blob);
+		form.append(pfx + 'fingerprint', this.chunkFingerprint(blob));
+		for (let i in this.postData)
+			form.append(i, this.postData[i]);
+
+		m.request({
+			method: 'POST',
+			url: this.url,
+			body: form,
+		}).then(resp => {
+			// check last chunk
+			if (this.chunkIndex >= this.chunkMax) {
+				if (!resp.data || !resp.data.done)
+					throw new ChuploadError(
+						E_UNFINISHED, "server failed to finish");
+				this.progress = -1;
+				this.cbFileOk(resp);
+				return;
+			}
+
+			// increment index
+			this.chunkIndex++;
+
+			// successful chunk upload
+			this.progress = ((this.chunkIndex / this.chunkMax) * 100)
+				.toString().replace(/\.([0-9]{2}).+/, '.$1');
+			this.cbChunkOk(resp);
+
+			// cancelled
+			if (this.cancelled)
+				return;
+
+			// progress
+			this.cbProgress(this.progress);
+
+			// continue to next chunk
+			this.uploadChunk();
+		}).catch(resp => {
+			this.cbError(resp.response);
+		});
+	}
+
+	cancel() {
+		this.cancelled = true;
+	}
+}
+
+
+const app = {
+	progress: 0,
+	file: null,
+	path: null,
+	errno: 0,
+
+	uploader: null,
+
+	reset() {
+		this.progress = 0;
+		this.file = null;
+		this.path = null;
+	},
+
+	getConfig() {
+		m.request(
+			'./config'
+		).then(resp => {
+			this.config = resp.data;
+		}).catch(() => {});
+	},
+
+	cbFileOk(resp) {
+		this.progress = 0;
+		this.file = null;
+		this.path = resp.data.path;
+		this.fileList();
+	},
+	cbError(resp) {
+		// FIXME: using this.error is always received as 0 by eleError
+		app.errno = resp.errno;
+		this.reset();
+	},
+	cbProgress(progress) {
+		this.progress = Number(progress);
+	},
+	upload() {
+		const cnf = this.config;
+		try {
+			this.uploader = new Chupload(
+				this.file, './upload',
+				cnf.post_prefix, cnf.chunk_size,
+				null,
+				resp => this.cbFileOk(resp),
+				resp => this.cbError(resp),
+				progress => this.cbProgress(progress), null, []
+			);
+			this.uploader.start();
+		} catch(err) {
+			this.errno = err.errno;
+		}
+	},
+	cancel() {
+		if (!this.uploader)
+			return;
+		this.uploader.cancel();
+		this.uploader = null;
+	},
+
+	eleFile() {
+		const self = this;
+		return m('input', {
+			type: 'file',
+			onchange() {
+				if (!this.files)
+					return;
+				self.reset();
+				self.file = this.files[0];
+			},
+		});
+	},
+	elePicker(upl) {
+		if (!this.file) {
+			return m('a', {
+				onclick() {
+					app.errno = 0;
+					upl.dom.click();
+				},
+			}, 'pick a file');
+		}
+		return m('span', this.file.name);
+	},
+
+	eleProgress() {
+		if (!this.progress)
+			return;
+		return [m('progress', {
+			max: 100,
+			value: this.progress,
+		}, this.progress + '%')];
+	},
+
+	btnReset() {
+		if (!this.file)
+			return;
+		const self = this;
+		return m('button', {
+			onclick() {
+				self.reset();
+			},
+		}, 'reset');
+	},
+	btnUpload(upl) {
+		if (!this.file)
+			return;
+		const self = this;
+		return m('button', {
+			onclick() {
+				self.upload(upl);
+			},
+		}, 'upload');
+	},
+	btnCancel() {
+		if (!this.progress || this.progress === 100)
+			return;
+		const self = this;
+		return m('button', {
+			onclick() {
+				self.cancel();
+				self.reset();
+			},
+		}, 'cancel');
+	},
+
+	elePath() {
+		if (!this.path)
+			return;
+		return [
+			m('span', 'OK:'),
+			m('a', {
+				href: './download/?fname=' + this.path,
+				target: '_blank',
+			}, this.path),
+		];
+	},
+
+	eleError() {
+		if (!app.errno)
+			return;
+		return [
+			m('span', 'ERROR with errno: 0x0' +
+				app.errno.toString(16) + '.'),
+		];
+	},
+
+	list: [],
+	fileList() {
+		m.request('./list').then(resp => {
+			this.list = resp.data;
+		}).catch(() => {});
+	},
+	fileRemove(ele) {
+		m.request({
+			method: 'DELETE',
+			url: './remove',
+			body: ele,
+			serialize(data) {
+				return data;
+			},
+		}).then(() => {}).catch(() => {}).finally(() => {
+			this.fileList();
+		});
+	},
+	fileEle(ele) {
+		const self = this;
+		return m('p', [
+			m('a', {
+				title: 'delete',
+				onclick() {
+					self.fileRemove(ele);
+				},
+			}, 'x'),
+			m('span', ' '),
+			m('a', {
+				href: './download?fname=' + ele,
+				target: '_blank',
+			}, ele),
+		]);
+	},
+
+	oninit() {
+		this.getConfig();
+		this.fileList();
+	},
+
+	tab: 'upload',
+	viewTab() {
+		const self = this;
+		return [
+			m('div.tab', [
+				m('span', {
+					onclick() {
+						self.tab = 'upload';
+					},
+					class: self.tab === 'upload' ? 'active' : '',
+				}, 'upload'),
+				m('span', {
+					onclick() {
+						self.tab = 'list';
+					},
+					class: self.tab === 'list' ? 'active' : '',
+				}, 'list'),
+				m('hr'),
+			]),
+		];
+	},
+	viewUpload() {
+		if (this.tab !== 'upload')
+			return;
+		const inp = this.eleFile();
+		return [
+			m('div.upload', [
+				inp,
+				this.elePicker(inp),
+				m('hr'),
+				this.btnReset(),
+				this.btnUpload(inp),
+				this.btnCancel(),
+				this.elePath(),
+				this.eleError(),
+			]),
+			m('div', this.eleProgress()),
+		];
+	},
+	viewList() {
+		if (this.tab !== 'list')
+			return;
+		return [
+			m('div.list', this.list.map(ele => this.fileEle(ele))),
+		];
+	},
+	view() {
+		if (!this.config)
+			return;
+		return m('div', [
+			this.viewTab(),
+			m('div', this.viewUpload(), this.viewList())
+		]);
+	},
+};
+
+
+// root
+m.mount(document.querySelector('#box'), app);
